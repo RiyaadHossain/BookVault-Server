b03d3d56aa3b574fe639fa54ff5a5fc3749f268a
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-07-17 08:23:25 +0600

    Review post, get, update, delete method added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 const cors = require('cors');
 
@@ -19,9 +19,12 @@ const run = async () => {
   try {
     const db = client.db('BookVault');
     const bookCollection = db.collection('books');
+    const reviewCollection = db.collection('reviews');
+    const wishlistCollection = db.collection('wishlists');
+    const readlistCollection = db.collection('readlists');
 
     app.get('/books', async (req, res) => {
-      const cursor = bookCollection.find({});
+      const cursor = bookCollection.find({}).sort({ publicationDate: -1 });
       const book = await cursor.toArray();
 
       res.send({ status: true, data: book });
@@ -44,7 +47,7 @@ const run = async () => {
     app.patch('/book/:id', async (req, res) => {
       const id = req.params.id;
       const updatedData = req.body
-      const result = await bookCollection.findOneAndUpdate({ _id: ObjectId(id) }, updatedData);
+      const result = await bookCollection.findOneAndUpdate({ _id: ObjectId(id) }, { $set: updatedData });
       res.send(result);
     });
 
@@ -57,7 +60,7 @@ const run = async () => {
 
     app.post('/review/:id', async (req, res) => {
       const bookId = req.params.id;
-      const review = req.body.review;
+      const review = req.body;
 
       const result = await bookCollection.updateOne(
         { _id: ObjectId(bookId) },
@@ -86,6 +89,83 @@ const run = async () => {
       } else {
         res.status(404).json({ error: 'Book not found' });
       }
+    });
+
+    app.patch('/review/:id/user/:email', async (req, res) => {
+      const bookId = req.params.id;
+      const userEmail = req.params.email
+      const updatedReview = req.body.review
+
+      const result = await bookCollection.findOneAndUpdate(
+        { _id: ObjectId(bookId), "reviews.userEmail": userEmail },
+        { $set: { "reviews.$.review": updatedReview } }
+      );
+
+      if (result) {
+        return res.json(result);
+      }
+
+      res.status(404).json({ error: 'Book not found' });
+    });
+
+    app.delete('/review/:id/user/:email', async (req, res) => {
+      const bookId = req.params.id;
+      const userEmail = req.params.email
+
+      const result = await bookCollection.findOneAndUpdate(
+        { _id: ObjectId(bookId) },
+        { $pull: { reviews: { userEmail } } }
+      );
+
+      if (result) {
+        return res.json(result);
+      }
+
+      res.status(404).json({ error: 'Book not found' });
+    });
+
+    app.post('/wishlist', async (req, res) => {
+      const { userEmail, book } = req.body;
+      const payload = { userEmail, books: [book] }
+
+      let result
+      const exist = await wishlistCollection.findOne({ userEmail })
+      if (exist)
+        result = await wishlistCollection.findOneAndUpdate({ userEmail }, { $push: { books: book } })
+      else
+        result = await wishlistCollection.insertOne(payload);
+
+      res.json({ message: 'Wishlist added successfully', result: result.value });
+    });
+
+    app.get('/wishlist/:email', async (req, res) => {
+      const userEmail = req.params.email;
+      const result = await wishlistCollection.findOne({ userEmail });
+
+      if (result) {
+        return res.json(result);
+      }
+
+      res.status(404).json({ error: 'Book not found' });
+
+    });
+
+    // ToDo: Fix This!
+    app.delete('/wishlist/:email/book/:bookId', async (req, res) => {
+      const userEmail = req.params.email;
+      const bookId = req.params.bookId;
+      console.log(bookId)
+      const result = await wishlistCollection.findOneAndUpdate(
+        { userEmail },
+        { $pull: { books: { _id: ObjectId(bookId) } } }
+      );
+
+      if (result) {
+        return res.json(result);
+      }
+
+      res.status(404).json({ error: 'Book not found' });
+
     });
 
     app.post('/user', async (req, res) => {
